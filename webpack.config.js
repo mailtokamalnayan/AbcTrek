c0f78ba6ea677ab22a1e9a6f413e4b20009d3773
mailtokamalnayan <mailtokamalnayan@gmail.com>
2017-11-18 17:18:32 +0530

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports={
+  context: __dirname + "/src",
+  entry: "./index",
+  output:{
+    filename:"app.js",
+    path:__dirname+"/demo"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  }
+}
